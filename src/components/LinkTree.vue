ca4477bd1874c1ae2156a1cb6e6e5802f05e08ca
calvinmth <63382223+calvinmth@users.noreply.github.com>
2025-08-17 23:49:00 +0700

    fix url issue

--- src/components/LinkTree.vue
+++ src/components/LinkTree.vue
@@ -1,5 +1,6 @@
 <template>
-    <v-card v-for="item in items" class="ma-5 rounded-xl elevation-3" @click="redirect(item.url)">
+    <v-card v-for="item in items" :key="item.title" class="ma-5 rounded-xl elevation-3" :href="item.url" target="_blank"
+        rel="noopener noreferrer">
         <v-card-title>
             <v-icon start :color="item.iconColor">{{ item.icon }}</v-icon>
             {{ item.title }}
@@ -16,29 +17,25 @@
 <script setup>
 const items = [
     {
-        'title': 'Spotify',
-        'subtitle': 'Visit calvinmth on Spotify',
-        'icon': 'mdi-spotify',
-        'iconColor': 'green',
-        'url': 'bit.ly/calvinmth-spotify',
+        title: 'Spotify',
+        subtitle: 'Visit calvinmth on Spotify',
+        icon: 'mdi-spotify',
+        iconColor: 'green',
+        url: 'https://bit.ly/calvinmth-spotify',
     },
     {
-        'title': 'Instagram',
-        'subtitle': 'Visit calvinmth on Instagram',
-        'icon': 'mdi-instagram',
-        'iconColor': 'pink-lighten-2',
-        'url': 'bit.ly/calvinmth-instagram',
+        title: 'Instagram',
+        subtitle: 'Visit calvinmth on Instagram',
+        icon: 'mdi-instagram',
+        iconColor: 'pink-lighten-2',
+        url: 'https://bit.ly/calvinmth-instagram',
     },
     {
-        'title': 'Youtube',
-        'subtitle': 'Visit calvinmth on Youtube',
-        'icon': 'mdi-youtube',
-        'iconColor': 'red',
-        'url': 'bit.ly/calvinmth-youtube',
+        title: 'Youtube',
+        subtitle: 'Visit calvinmth on Youtube',
+        icon: 'mdi-youtube',
+        iconColor: 'red',
+        url: 'https://bit.ly/calvinmth-youtube',
     },
 ];
-
-function redirect(url) {
-    window.location.href = url;
-};
 </script>
